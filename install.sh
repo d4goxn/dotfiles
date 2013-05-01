cc4ac61ba95d19dfa85b807b27c13367f3b16ca4
Dan Ross <ross9885@gmail.com>
2013-04-30 23:48:58 -0700

    Added Syntastic and an install script to get dependencies.

---
+++ install.sh
@@ -0,0 +1,18 @@
+# CSS
+# LESS
+# JSON
+npm install -g git://github.com/stubbornella/csslint.git csslint &&
+npm install -g less git://github.com/zaach/jsonlint.git jsonlint &&
+
+# C
+# CPP
+# PHP
+sudo apt-get install cabal-install pyhton-pip splint cppcheck php-codesniffer &&
+
+ # Haskel
+cabal install hdevtools &&
+
+ # Javascript
+sudo easy_install http://closure-linter.googlecode.com/files/closure_linter-latest.tar.gz &&
+
+sudo pip install flake8 # Python
